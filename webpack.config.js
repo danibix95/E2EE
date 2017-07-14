46f3b50fd9cfe39d22ed0b0a11d6e00dd3970468
danibix95 <dani95bissoli@gmail.com>
2017-07-14 16:16:23 +0200

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'e2ee.js'
+  }
+};
