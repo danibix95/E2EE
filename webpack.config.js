cee62713273d9ec904d9c25e01e3826233ad038b
danibix95 <dani95bissoli@gmail.com>
2017-07-28 15:43:37 +0200

    Updated encryption layer and package config.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'e2ee.js'
+    filename: 'e2ee.js',
+    libraryTarget: 'window',
+    library: 'SBOX'
   }
 };
