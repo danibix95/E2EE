be9b35c7fc97d65c3b148f6754e1f7f331c5fc38
danibix95 <dani95bissoli@gmail.com>
2017-08-03 12:09:21 +0200

    Updated the script that generate data structures on Chino

--- genDataStructures.js
+++ genDataStructures.js
@@ -3,6 +3,8 @@
 /*
    This script is meant for creating
    required data structures on Chino server
+
+   Note: this script will remove everything is on your space on Chino Server
 */
 
 const API = require("./lib/api_connect");
@@ -12,6 +14,9 @@ const auth = {
   secret: process.env.CHINO_KEY   // change with your Chino Customer Key
 }
 
+const cleanTheEnvironment = true; // decide if clean your Chino environment
+
+/* NEEDED STRUCTURES */
 const appData = {
   name: "Safe Deposit Application",
   grant_type: "password",
@@ -126,11 +131,11 @@ const ks = {
 }
 
 const sbm = {
-  description: "SBox Messages",
+  description: "SBox Documents",
   structure: {
     fields: [
       {
-        name: "enc_message",
+        name: "enc_doc",
         type: "base64",
       },
       {
@@ -141,6 +146,10 @@ const sbm = {
       {
         name: "writer_id",
         type: "string"
+      },
+      {
+        name: "ad",
+        type: "json"
       }
     ]
   }
@@ -172,21 +181,89 @@ const sbf = {
   }
 }
 
+/* DELETE EVERYTHING ON CHINO - IF REQUIRED */
+if (cleanTheEnvironment) {
+  Promise.all([
+    API.get(`/auth/applications`, {}, auth)
+        .then(result =>
+            Promise.all(result.data.applications.map(
+                app => API.del(`/auth/applications/${app.app_id}`, {}, auth)
+                )
+            )
+        )
+        .then((result) => {
+          result.forEach(res => console.log(res.result_code));
+        }),
+    API.get(`/collections`, {}, auth)
+        .then(result =>
+            Promise.all(result.data.collections.map(
+                c => API.del(`/collections/${c.collection_id}`, {force:true}, auth)
+                )
+            )
+        )
+        .then((result) => {
+          result.forEach(res => console.log(res.result_code));
+        }),
+    API.get(`/groups`, {}, auth)
+        .then(result =>
+            Promise.all(result.data.groups.map(g => API.del(`/groups/${g.group_id}`, {force:true}, auth)))
+        )
+        .then((result) => {
+          result.forEach(res => console.log(res.result_code));
+        }),
+    API.get(`/repositories`, {}, auth)
+        .then(result =>
+            Promise.all(
+                result.data.repositories.map(
+                    repo => API.get(`/repositories/${repo.repository_id}/schemas`, {offset : 0, limit : 50}, auth)
+                        .then(res => Promise.all(res.data.schemas.map(
+                            schema => API.del(`/schemas/${schema.schema_id}`, {force : true, all_content : true}, auth)))
+                        )
+                        .then(res => API.del(`/repositories/${repo.repository_id}`, {force:true}, auth))
+                        .catch(err => console.log(err))
+                )
+            )
+        )
+        .then((result) => {
+          result.forEach(res => console.log(res.result_code));
+        }),
+    API.get(`/user_schemas`, {}, auth)
+        .then(result =>
+            Promise.all(
+                result.data.user_schemas.map(
+                    us => API.del(`/user_schemas/${us.user_schema_id}`, {force:true}, auth)
+                )
+            )
+        )
+        .then((result) => {
+          result.forEach(res => console.log(res.result_code));
+        })
+  ])
+      .then(() => {console.log("Chino environment cleaned"); return uploadStructure() })
+      .catch(error => { console.log(error); });
+}
+else {
+  uploadStructure();
+}
+
 // CALLS
-API.post("/auth/applications", appData, auth)
-    .then((r) => {console.log(r);})
-    .catch((e) => {console.error(e)});
-API.post("/user_schemas", userSchema, auth)
-    .then((r) => {console.log(r);})
-    .catch((e) => {console.error(e)});
-API.post("/repositories", repoSBox, auth)
-    .then((r) => {console.log(r); dataId = r.data.repository.repository_id})
-    .then(() => API.post("/repositories", repoOther, auth))
-    .then((r) => {console.log(r); otherId = r.data.repository.repository_id})
-    .then(() => API.post(`/repositories/${otherId}/schemas`, usbox, auth))
-    .then((r) => {console.log(r); return API.post(`/repositories/${otherId}/schemas`, pubKeys, auth)})
-    .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, sbox, auth)})
-    .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, ks, auth)})
-    .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, sbm, auth)})
-    .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, sbf, auth)})
-    .catch((e) => {console.error(e)});
+function uploadStructure() {
+  return Promise.all([
+    API.post("/auth/applications", appData, auth)
+        .then((r) => {console.log(r);}),
+    API.post("/user_schemas", userSchema, auth)
+        .then((r) => {console.log(r);}),
+    API.post("/repositories", repoSBox, auth)
+        .then((r) => {console.log(r); dataId = r.data.repository.repository_id})
+        .then(() => API.post("/repositories", repoOther, auth))
+        .then((r) => {console.log(r); otherId = r.data.repository.repository_id})
+        .then(() => API.post(`/repositories/${otherId}/schemas`, usbox, auth))
+        .then((r) => {console.log(r); return API.post(`/repositories/${otherId}/schemas`, pubKeys, auth)})
+        .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, sbox, auth)})
+        .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, ks, auth)})
+        .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, sbm, auth)})
+        .then((r) => {console.log(r); return API.post(`/repositories/${dataId}/schemas`, sbf, auth)})
+  ])
+      .then(() => console.log("\nNeeded data structures successfully created on Chino server"))
+      .catch((e) => {console.error(e)});
+}
